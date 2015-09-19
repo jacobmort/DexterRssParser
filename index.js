22cf6ed0fffac3f94aa0ebce41918019d9c428a3
Jacob Mortensen <jacob@yourmemoir.com>
2015-09-19 13:41:15 -0400

    copied from user docs

--- index.js
+++ index.js
@@ -1,13 +1,79 @@
+var request = require('request');
+var FeedParser = require('feedparser');
+var _ = require ('lodash');
 module.exports = {
-    /**
-     * The main entry point for the Dexter module
-     *
-     * @param {AppStep} step Accessor for the configuration for the step using this module.  Use step.input('{key}') to retrieve input data.
-     * @param {AppData} dexter Container for all data used in this workflow.
-     */
-    run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+    run: function(step) {
+        var url = step.input('url').first(),
+          filter = step.input('filter').first(),
+          self = this;
+        this.fetchUrl(url, function(err, stream) {
+            if(err) {
+                return self.fail(err);
+            }
+            //Let the parser grab the data
+            self.fetchItems(stream, function(err, items) {
+                var response = [];
+                if(err) {
+                    return self.fail(err);
+                }
+                //Extract dexter-friendly data from the items
+                _.each(items, function(item) {
+                    response.push(
+                      url
+                      //{
+                      //  url: item.link,
+                      //  title: item.title,
+                      //  summary: item.summary,
+                      //  author: item.author
+                      //}
+                    );
+                });
+                return self.complete(response);
+            });
+        });
+    },
+    fetchUrl: function(url, callback) {
+        var req = request(url);
+        req.on('error', callback);
+        req.on('response', function(resp) {
+            if(resp.statusCode != 200) {
+                return callback(new Error(
+                  'Bad status code ' + resp.statusCode + ' for ' + url
+                ));
+            }
+            return callback(null, this);
+        });
+    },
+    fetchItems: function(stream, callback) {
+        var parser = new FeedParser()
+          , items = [];
+        parser.on('error', callback);
+        parser.on('end', function(err) {
+            if(err) {
+                return callback(err);
+            }
+            return callback(null, items);
+        });
+        parser.on('readable', function() {
+            var item;
+            while((item = this.read())) {
+                items.push(item);
+            }
+        });
+        stream.pipe(parser);
+    },
+
+    filterItems: function(items, term) {
+        var response = []
+          , lcTerm = term.toLowerCase();
+        _.each(items, function(item) {
+            _.forIn(item, function(val) {
+                if(val.toLowerCase().indexOf(lcTerm) >= 0) {
+                    response.push(item);
+                    return false;
+                }
+            });
+        });
+        return response;
     }
-};
+}
